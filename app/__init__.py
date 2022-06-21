80618e1f518323da2defb8ddda0d8e326d49a9eb
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-06-21 19:30:59 -0400

    rm map implementation

--- app/__init__.py
+++ app/__init__.py
@@ -34,35 +34,3 @@ def experience():
         url=os.getenv("URL"),
         data=data,
     )
-
-
-@app.route("/map")
-def map():
-    return render_template(
-        "map.html", title="Sam Thibault - Portfolio", url=os.getenv("URL"), data=data
-    )
-
-
-@app.route("/folium_map")
-def folium_map():
-
-    start_coords = (43.653, -79.383)
-    folium_map = folium.Map(location=start_coords, zoom_start=3, tiles="Stamen Terrain")
-
-    for i in range(0, len(data["locations"])):
-        folium.Marker(
-            location=[data["locations"][i]["latitude"], data["locations"][i]["longitude"]],
-            popup=data["locations"][i]["name"],
-            icon=folium.Icon(color=data["locations"][i]["color"]),
-        ).add_to(folium_map)
-
-    ## fix mobile view
-    folium_map.get_root().header.add_child(
-        folium.Element(
-            '<meta name="viewport" content="width=device-width,'
-            ' initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />'
-        )
-    )
-
-    folium_map.save("templates/map.html")
-    return render_template("mapindex.html")
