2efc95311cd5561722712e1f915159bb914e6678
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-06-22 11:10:58 -0400

    connect db and api routes added

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import json
 from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 
 load_dotenv()
@@ -10,7 +12,7 @@ app = Flask(__name__)
 data = open("static/data.json")
 data = json.load(data)
 
-# connect to the local MySQL db using peewee
+# connect to the local MySQL db using peewee and environment variables
 mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
@@ -20,6 +22,17 @@ mydb = MySQLDatabase(
 )
 
 print(mydb)
+
+# peewee model for the timeline posts
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
 
 # routes send the loaded json object "data" to display personal information
 @app.route("/")
@@ -44,3 +57,23 @@ def experience():
         url=os.getenv("URL"),
         data=data,
     )
+
+
+@app.route("/api/timeline_post", methods=["POST"])
+def post_time_line_post():
+    name = request.form["name"]
+    email = request.form["email"]
+    content = request.form["content"]
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+
+@app.route("/api/timeline_post", methods=["GET"])
+def get_time_line_post():
+    return {
+        "timeline_posts": [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
