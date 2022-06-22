9c59c25b093b33d5877055ed21e896524af7c188
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-06-22 10:58:02 -0400

    db test success

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,24 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import json
+from peewee import *
+
 
 load_dotenv()
 app = Flask(__name__)
 data = open("static/data.json")
 data = json.load(data)
 
+# connect to the local MySQL db using peewee
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306,
+)
+
+print(mydb)
 
 # routes send the loaded json object "data" to display personal information
 @app.route("/")
