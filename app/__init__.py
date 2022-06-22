f60d1fd47d6bfbc661a5fc6fc5353f29144e8904
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-06-22 12:20:49 -0400

    api routes added

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 data = open("static/data.json")
 data = json.load(data)
 
-# connect to the local MySQL db using peewee and environment variables
+# MySQL db variable using peewee and environment variables
 mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
@@ -20,8 +20,6 @@ mydb = MySQLDatabase(
     host=os.getenv("MYSQL_HOST"),
     port=3306,
 )
-
-print(mydb)
 
 # peewee model for the timeline posts
 class TimelinePost(Model):
@@ -34,6 +32,12 @@ class TimelinePost(Model):
         database = mydb
 
 
+# connect to the database, and create a table using the above model
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
+##### FRONTEND ROUTES #####
 # routes send the loaded json object "data" to display personal information
 @app.route("/")
 def index():
@@ -59,16 +63,23 @@ def experience():
     )
 
 
+##### TIMELINE API ROUTES #####
+# add a document by specifying field values in the request body
 @app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
+
     name = request.form["name"]
+    print(name)
     email = request.form["email"]
+    print(email)
     content = request.form["content"]
+    print(content)
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
     return model_to_dict(timeline_post)
 
 
+# get all documents
 @app.route("/api/timeline_post", methods=["GET"])
 def get_time_line_post():
     return {
@@ -77,3 +88,20 @@ def get_time_line_post():
             for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
+
+
+# delete a document by name
+@app.route("/api/timeline_post", methods=["DELETE"])
+def delete_time_line_post():
+    nameToDelete = request.form["name"]
+    qry = TimelinePost.delete().where(TimelinePost.name == nameToDelete)
+    qry.execute()
+    return "deleted: " + nameToDelete
+
+
+# this mapping deletes all documents from the database (used for testing)
+@app.route("/api/timeline_post/purge", methods=["DELETE"])
+def delete_all():
+    qry = TimelinePost.delete()
+    qry.execute()
+    return "deleted all rows"
