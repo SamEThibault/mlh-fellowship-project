1c778f75e3b4aff2ab7c7ad57f8095dfcb814cba
SamEThibault <sam5thibault@gmail.com>
2022-06-23 04:55:22 +0000

    change redeploy.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,8 +3,9 @@
 echo "killing existing tmux sessions..."
 tmux kill-server
 
+# Do this if you want to run on port 80 to hide port in URL: (also add export FLASK_RUN_PORT=80 to tmux commands)
 #this ensures port:80 is not in use since we want to host the site through the default http port
-sudo fuser -n tcp -k 80
+# sudo fuser -n tcp -k 80
 
 echo "fetching most recent repository changes..."
 cd /home/GitHub/mlh-fellowship-project/app
@@ -16,7 +17,7 @@ pip3 install -r /home/GitHub/mlh-fellowship-project/requirements.txt
 
 echo "starting tmux session..."
 cd /home/GitHub/mlh-fellowship-project/app
-tmux new-session -d -s flask_portfolio 'source /home/GitHub/mlh-fellowship-project/python3-virtualenv/bin/activate && export FLASK_APP=__init__.py && export FLASK_RUN_PORT=80 && flask run --host=0.0.0.0; $SHELL'
+tmux new-session -d -s flask_portfolio 'source /home/GitHub/mlh-fellowship-project/python3-virtualenv/bin/activate && export FLASK_APP=__init__.py && flask run --host=0.0.0.0; $SHELL'
 
 echo "current tmux sessions:"
 tmux ls
