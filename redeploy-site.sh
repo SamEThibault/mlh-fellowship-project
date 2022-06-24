d51f9bb3fb3827bc627dc81b9fe2e75ff53ba69e
SamEThibault <sam5thibault@gmail.com>
2022-06-24 15:01:12 +0000

    modify redeploy script to use service instead

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "killing existing tmux sessions..."
-tmux kill-server
-
-# Do this if you want to run on port 80 to hide port in URL: (also add export FLASK_RUN_PORT=80 to tmux commands)
-#this ensures port:80 is not in use since we want to host the site through the default http port
-# sudo fuser -n tcp -k 80
-
 echo "fetching most recent repository changes..."
 cd /home/GitHub/mlh-fellowship-project/app
 git fetch && git reset origin/main --hard
@@ -15,12 +8,5 @@ echo "activating virtual environment..."
 source /home/GitHub/mlh-fellowship-project/python3-virtualenv/bin/activate
 pip3 install -r /home/GitHub/mlh-fellowship-project/requirements.txt
 
-echo "starting tmux session..."
-cd /home/GitHub/mlh-fellowship-project/app
-tmux new-session -d -s flask_portfolio 'source /home/GitHub/mlh-fellowship-project/python3-virtualenv/bin/activate && export FLASK_APP=__init__.py && flask run --host=0.0.0.0; $SHELL'
-
-echo "current tmux sessions:"
-tmux ls
-echo "flask application started"
-echo "Complete"
-
+systemctl restart myportfolio
+systemctl status myportfolio
