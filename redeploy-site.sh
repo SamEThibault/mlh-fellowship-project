618722b1d1512e58c190dc5b0208384db438bcdf
SamEThibault <sam5thibault@gmail.com>
2022-06-24 15:04:34 +0000

    fix redeploy path

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "fetching most recent repository changes..."
-cd /home/GitHub/mlh-fellowship-project/app
+cd /root/GitHub/mlh-fellowship-project/app
 git fetch && git reset origin/main --hard
 
 echo "activating virtual environment..."
-source /home/GitHub/mlh-fellowship-project/python3-virtualenv/bin/activate
-pip3 install -r /home/GitHub/mlh-fellowship-project/requirements.txt
+source /root/GitHub/mlh-fellowship-project/python3-virtualenv/bin/activate
+pip3 install -r /root/GitHub/mlh-fellowship-project/requirements.txt
 
 systemctl restart myportfolio
 systemctl status myportfolio
