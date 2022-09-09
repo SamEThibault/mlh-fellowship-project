0468edf2835d062e6c72d067c457ee75dc0df8a2
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-09-09 12:42:04 -0400

    edit redeploy script

--- shell_scripts/redeploy-site.sh
+++ shell_scripts/redeploy-site.sh
@@ -2,11 +2,11 @@
 
 echo "fetching most recent repository changes..."
 cd /root/github/mlh-fellowship-project
-git fetch && git reset origin/main --hard
+git checkout deployment && git pull
 
 echo "Spinning down existing containers to conserve memory..."
-docker compose -f docker-compose.home.yml down
+sudo docker-compose -f docker-compose.home.yml down
 
 echo "Re-building and spinning up application..."
-docker compose -f docker-compose.home.yml up -d --build
+sudo docker-compose -f docker-compose.home.yml up -d --build
 
