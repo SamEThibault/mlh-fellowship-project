e51573bf9590e0c87f08be9c368ae0599b3ea20d
SamEThibault <sam5thibault@gmail.com>
2022-09-19 02:06:11 -0400

    update deploy script for new server

--- shell_scripts/redeploy-site.sh
+++ shell_scripts/redeploy-site.sh
@@ -5,8 +5,8 @@ cd /home/opc/mlh-fellowship-project
 git checkout deployment && git pull
 
 echo "Spinning down existing containers to conserve memory..."
-sudo docker-compose -f docker-compose.home.yml down
+sudo docker compose -f docker-compose.home.yml down
 
 echo "Re-building and spinning up application..."
-sudo docker-compose -f docker-compose.home.yml up -d --build
+sudo docker compose -f docker-compose.home.yml up -d --build
 
